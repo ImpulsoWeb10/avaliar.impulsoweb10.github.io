54d215e529dbec9fe73c487d290a1019bf8c92eb
ImpulsoWeb10 <rafagds130303@gmail.com>
2026-03-18 00:12:45 -0300

    Update clientes.js

--- clientes.js
+++ clientes.js
@@ -1,1 +1,48 @@
 
+const clientes = [
+  {
+    nome: "Óptica Lionda",
+    pasta: "opticalionda",
+    logo: "logo-optica-lionda.png"
+  },
+  {
+    nome: "Ana Liliam Cake",
+    pasta: "analiliamcake",
+    logo: "logoanaliliamcake.png"
+  },
+  {
+    nome: "QualyQuimy",
+    pasta: "qualyquimy",
+    logo: "logotintasqualyquimy.png"
+  },
+  {
+    nome: "Esquadriebox",
+    pasta: "esquadriebox",
+    logo: "esquadri-e-box.png"
+  },
+  {
+    nome: "Amor Pro Patinhas",
+    pasta: "amorpropatinhas",
+    logo: "logo.png"
+  },
+  {
+    nome: "Fretadão",
+    pasta: "fretadao",
+    logo: "logo.png"
+  },
+  {
+    nome: "Itapuã Center",
+    pasta: "itapua-center",
+    logo: "logo.png"
+  },
+  {
+    nome: "Lelidecor",
+    pasta: "lelidecor",
+    logo: "logo.png"
+  },
+  {
+    nome: "Luciano Cabeleireiro",
+    pasta: "luciano-cabeleireiro",
+    logo: "logo.png"
+  }
+];
