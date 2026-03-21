0a2b7209fe3db942219ce1c37a4cad018ae20cb2
ImpulsoWeb10 <rafagds130303@gmail.com>
2026-03-21 02:28:12 -0300

    Update clientes.js

--- clientes.js
+++ clientes.js
@@ -72,7 +72,7 @@ const clientes = [
   {
 nome: "lidia lindona",
 pasta: "lidia",
-logo: "https://unsplash.com/pt-br/fotografias/uma-figura-solitaria-esta-em-pe-em-uma-duna-de-areia-ao-por-do-sol-gzxxKNmrwHY",
+logo: "https://images.unsplash.com/photo-1773061941695-43568f71bee9?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 subtitulo: "",
 instagram: "@TINTASQUALYQUIMY",
 facebook: ""
